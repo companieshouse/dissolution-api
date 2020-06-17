7574f0a2f29f0e5a325cf4de06632e7378d378a0
Peadar Kelly <peadark@kainos.com>
2020-06-17 12:19:53 +0100

    Removed logger and added mongo to compose

--- src/main/java/uk/gov/companieshouse/DissolutionApplication.java
+++ src/main/java/uk/gov/companieshouse/DissolutionApplication.java
@@ -1,16 +1,12 @@
 package uk.gov.companieshouse;
 
-import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DissolutionApplication {
 
-    private static final Logger LOGGER = org.slf4j.LoggerFactory.getLogger(DissolutionApplication.class);
-
     public static void main (String[] args) {
-        LOGGER.info("Starting Dissolution API...");
         SpringApplication.run(DissolutionApplication.class, args);
     }
 }
