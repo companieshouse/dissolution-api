607d740a5c6f066ff37a3cdf65af3ef349eaafcc
Peadar Kelly <peadark@kainos.com>
2020-08-10 17:59:37 +0100

    Replaced SpringBootTest with MockMvc

--- src/test/java/uk/gov/companieshouse/controller/SubmitControllerTest.java
+++ src/test/java/uk/gov/companieshouse/controller/SubmitControllerTest.java
@@ -1,10 +1,11 @@
 package uk.gov.companieshouse.controller;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import uk.gov.companieshouse.service.dissolution.chips.DissolutionChipsService;
 
@@ -12,8 +13,8 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+@WebMvcTest(SubmitController.class)
 public class SubmitControllerTest {
 
     private static final String SUBMIT_URI = "/dissolution-request/submit";
