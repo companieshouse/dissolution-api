18c16cc5fd69c8ff954a8b979a9591b37fb4deb4
Carl Fagg <cfagg@PD15888.local>
2020-09-21 14:07:24 +0100

    S4-234: Added support for exapnded list fo officer roles + Corporate Director + Corporate Nominee Director + Judical Factor + LLP Designated Member + Corporate LLP Member + Corporate LLP Designated Member

--- src/main/java/uk/gov/companieshouse/model/enums/OfficerRole.java
+++ src/main/java/uk/gov/companieshouse/model/enums/OfficerRole.java
@@ -4,8 +4,14 @@ import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum OfficerRole {
     DIRECTOR("director"),
+    CORPORATE_DIRECTOR("corporate-director"),
+    CORPORATE_NOMINEE_DIRECTOR("corporate-nominee-director"),
+    JUDICIAL_FACTOR("judicial-factor"),
     SECRETARY("secretary"),
-    LLP_MEMBER("llp-member");
+    LLP_MEMBER("llp-member"),
+    LLP_DESIGNATED_MEMBER("llp-designated-member"),
+    CORPORATE_LLP_MEMBER("corporate-llp-member"),
+    CORPORATE_LLP_DESIGNATED_MEMBER("corporate-llp-designated-member");
 
     private final String value;
 
