86eb3d411a7ba7869f6038409ec38693f0895b67
Matthew O'Reilly <m.oreilly@kainos.com>
2020-10-14 16:13:08 +0100

    feature/S4-285: Support additional company types.

--- src/main/java/uk/gov/companieshouse/model/enums/CompanyType.java
+++ src/main/java/uk/gov/companieshouse/model/enums/CompanyType.java
@@ -6,7 +6,15 @@ public enum CompanyType {
     PLC("plc"),
     LTD("ltd"),
     LLP("llp"),
-    EEIG("European economic interest grouping (EEIG)");
+    PRIVATE_UNLIMITED("private-unlimited"),
+    OLD_PUBLIC_COMPANY("old-public-company"),
+    PRIVATE_LIMITED_GUARANT_NSC_LIMITED_EXEMPTION("private-limited-guarant-nsc-limited-exemption"),
+    PRIVATE_LIMITED_GUARANT_NSC("private-limited-guarant-nsc"),
+    PRIVATE_UNLIMITED_NSC("private-unlimited-nsc"),
+    PRIVATE_LIMITED_SHARES_SECTION_30_EXEMPTION("private-limited-shares-section-30-exemption"),
+    NORTHERN_IRELAND("northern-ireland"),
+    NORTHERN_IRELAND_OTHER("northern-ireland-other"),
+    EEIG("european-economic-interest-grouping");
 
     private final String value;
 
