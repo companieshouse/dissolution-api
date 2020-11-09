119482477fc3bbe3c7581c1e266a86f6d6417f2a
Matthew O'Reilly <m.oreilly@kainos.com>
2020-11-09 16:13:41 +0000

    Revert "feature/BI-5808: Add interceptor to handle endpoints that have OAuth OR API key authentication."
    This reverts commit 9f5ff0347509b015c391f49ad149135f0efe7535.

--- src/main/java/uk/gov/companieshouse/config/SecurityConfig.java
+++ src/main/java/uk/gov/companieshouse/config/SecurityConfig.java
@@ -1,16 +1,13 @@
 package uk.gov.companieshouse.config;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import uk.gov.companieshouse.api.interceptor.InternalUserInterceptor;
 import uk.gov.companieshouse.api.interceptor.TokenPermissionsInterceptor;
-import uk.gov.companieshouse.interceptor.TokenOrApiKeyPermissionsInterceptor;
 import uk.gov.companieshouse.interceptor.DissolutionTokenPermissionsInterceptor;
-
-import java.util.Arrays;
-import java.util.stream.Stream;
 
 @Configuration
 public class SecurityConfig implements WebMvcConfigurer {
@@ -18,20 +15,15 @@ public class SecurityConfig implements WebMvcConfigurer {
     private static final String URI_PATTERN = "/dissolution-request/**";
 
     private static final String[] API_KEY_PERMISSION_AUTH_INCLUDE_LIST = {
-        "/dissolution-request/submit",
-        "/dissolution-request/response"
+            "/dissolution-request/{application-reference}/payment",
+            "/dissolution-request/submit",
+            "/dissolution-request/response"
     };
 
-    private static final String[] TOKEN_OR_API_KEY_PERMISSION_AUTH_INCLUDE_LIST = {
-        "/dissolution-request/{application-reference}/payment"
-    };
-
-    private static final String[] TOKEN_PERMISSION_AUTH_EXCLUDE_LIST =
-        Stream.of(
-                Arrays.stream(API_KEY_PERMISSION_AUTH_INCLUDE_LIST),
-                Arrays.stream(TOKEN_OR_API_KEY_PERMISSION_AUTH_INCLUDE_LIST),
-                "/dissolution-request/healthcheck"
-        ).toArray(String[]::new);
+    private static final String[] TOKEN_PERMISSION_AUTH_EXCLUDE_LIST = ArrayUtils.addAll(
+        API_KEY_PERMISSION_AUTH_INCLUDE_LIST,
+        "/dissolution-request/healthcheck"
+    );
 
     @Autowired
     private TokenPermissionsInterceptor tokenPermissionsInterceptor;
@@ -42,14 +34,10 @@ public class SecurityConfig implements WebMvcConfigurer {
     @Autowired
     private InternalUserInterceptor apiKeyPermissionsInterceptor;
 
-    @Autowired
-    private TokenOrApiKeyPermissionsInterceptor tokenOrApiKeyPermissionsInterceptor;
-
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
         registry.addInterceptor(tokenPermissionsInterceptor).addPathPatterns(URI_PATTERN).excludePathPatterns(TOKEN_PERMISSION_AUTH_EXCLUDE_LIST);
         registry.addInterceptor(dissolutionTokenPermissionsInterceptor).addPathPatterns(URI_PATTERN).excludePathPatterns(TOKEN_PERMISSION_AUTH_EXCLUDE_LIST);
         registry.addInterceptor(apiKeyPermissionsInterceptor).addPathPatterns(API_KEY_PERMISSION_AUTH_INCLUDE_LIST);
-        registry.addInterceptor(tokenOrApiKeyPermissionsInterceptor).addPathPatterns(TOKEN_OR_API_KEY_PERMISSION_AUTH_INCLUDE_LIST);
     }
 }
