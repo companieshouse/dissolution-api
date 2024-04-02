78886aa6c7a6464cd77f89e72c0d8fdd6e6af57f
gjackson-ch <gjackson@companieshouse.gov.uk>
2024-04-02 16:20:52 +0100

    Updates for multilb

--- terraform/groups/ecs-service/data.tf
+++ terraform/groups/ecs-service/data.tf
@@ -34,11 +34,20 @@ data "aws_iam_role" "ecs_cluster_iam_role" {
 }
 
 data "aws_lb" "service_lb" {
-  name = "${var.environment}-chs-accountchgovuk"
+  name = "${var.environment}-chs-apichgovuk"
 }
 
 data "aws_lb_listener" "service_lb_listener" {
   load_balancer_arn = data.aws_lb.service_lb.arn
+  port              = 443
+  }
+
+data "aws_lb" "secondary_lb" {
+  name = "${var.environment}-chs-apichgovuk-private"
+}
+
+data "aws_lb_listener" "secondary_lb_listener" {
+  load_balancer_arn = data.aws_lb.secondary_lb.arn
   port              = 443
 }
 
