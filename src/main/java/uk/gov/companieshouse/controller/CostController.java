88e2fd665231e04bcf0c15d939214f095910f6f9
ParjalRai-CH <prai@companieshouse.gov.uk>
2026-07-24 14:04:28 +0100

    add api responses to getCosts

--- src/main/java/uk/gov/companieshouse/controller/CostController.java
+++ src/main/java/uk/gov/companieshouse/controller/CostController.java
@@ -1,9 +1,14 @@
 package uk.gov.companieshouse.controller;
-import org.springframework.http.ResponseEntity;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.companieshouse.api.model.payment.Cost;
 import uk.gov.companieshouse.exception.DissolutionNotFoundException;
@@ -21,7 +26,13 @@ public class CostController {
         this.costService = costService;
     }
 
+    @Operation(summary = "Get Dissolution Costs", tags = "Dissolution")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Costs Retrieved"),
+            @ApiResponse(responseCode = "404", description = "Dissolution Application not found")
+    })
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Object> getCosts(
             @PathVariable("transaction_id") String transactionId,
             @PathVariable("dissolution_id") String dissolutionId) throws DissolutionNotFoundException {
