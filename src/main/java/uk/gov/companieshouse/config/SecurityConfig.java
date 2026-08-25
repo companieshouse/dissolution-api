97ccc88b0f23d45fdc95270648c55fa2eeb206c0
gsraich <gsrai@companieshouse.gov.uk>
2026-08-25 09:38:21 +0100

    Add approval endpoint to ecs alb terraform config

--- src/main/java/uk/gov/companieshouse/config/SecurityConfig.java
+++ src/main/java/uk/gov/companieshouse/config/SecurityConfig.java
@@ -15,8 +15,8 @@ public class SecurityConfig implements WebMvcConfigurer {
     private static final String URI_PATTERN = "/dissolution-request/**";
     private static final String FILINGS = "/private/transactions/**/filings";
     private static final String COSTS = "/transactions/**/costs";
-    private static final String DISSOLUTION = "/company/**/transaction/**/dissolution";
-    private static final String DISSOLUTION_APPROVAL = "/approve";
+    private static final String DISSOLUTION = "/company/*/transaction/*/dissolution";
+    private static final String DISSOLUTION_APPROVAL = DISSOLUTION + "/approve";
 
     private static final String[] API_KEY_PERMISSION_AUTH_INCLUDE_LIST = {
             "/dissolution-request/{application-reference}/payment",
@@ -36,7 +36,7 @@ public class SecurityConfig implements WebMvcConfigurer {
             "/transactions/**",
             FILINGS,
             DISSOLUTION,
-            DISSOLUTION + DISSOLUTION_APPROVAL
+            DISSOLUTION_APPROVAL
     };
 
     private final TokenPermissionsInterceptor tokenPermissionsInterceptor;
@@ -57,8 +57,8 @@ public class SecurityConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
-        registry.addInterceptor(tokenPermissionsInterceptor).addPathPatterns(URI_PATTERN, DISSOLUTION, DISSOLUTION + DISSOLUTION_APPROVAL).excludePathPatterns(TOKEN_PERMISSION_AUTH_EXCLUDE_LIST);
-        registry.addInterceptor(dissolutionTokenPermissionsInterceptor).addPathPatterns(URI_PATTERN, DISSOLUTION, DISSOLUTION + DISSOLUTION_APPROVAL).excludePathPatterns(TOKEN_PERMISSION_AUTH_EXCLUDE_LIST);
+        registry.addInterceptor(tokenPermissionsInterceptor).addPathPatterns(URI_PATTERN, DISSOLUTION, DISSOLUTION_APPROVAL).excludePathPatterns(TOKEN_PERMISSION_AUTH_EXCLUDE_LIST);
+        registry.addInterceptor(dissolutionTokenPermissionsInterceptor).addPathPatterns(URI_PATTERN, DISSOLUTION, DISSOLUTION_APPROVAL).excludePathPatterns(TOKEN_PERMISSION_AUTH_EXCLUDE_LIST);
         registry.addInterceptor(apiKeyPermissionsInterceptor).addPathPatterns(API_KEY_PERMISSION_AUTH_INCLUDE_LIST);
         registry.addInterceptor(transactionInterceptor).addPathPatterns(TRANSACTIONS_INCLUDE_LIST);
     }
