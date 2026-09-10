0b22a3f716ccdb5d1b88770b857f5aa94dfb9d11
gsraich <gsrai@companieshouse.gov.uk>
2026-09-10 11:41:21 +0100

    fix: use base path const in test

--- src/test/java/uk/gov/companieshouse/service/dissolution/DissolutionServiceTest.java
+++ src/test/java/uk/gov/companieshouse/service/dissolution/DissolutionServiceTest.java
@@ -436,7 +436,7 @@ class DissolutionServiceTest {
         @Test
         void when_submitted_dissolution_exists_with_verdict_then_draft_dissolution_created() {
             final Transaction transaction = aTransaction().withStatus(TransactionStatus.OPEN).withCompanyNumber(COMPANY_NUMBER).build();
-            final String expectedSelfLink = String.format("/company/%s/transaction/%s/dissolution", COMPANY_NUMBER, transaction.getId());
+            final String expectedSelfLink = String.format(DISSOLUTION_BASE_URI_PATTERN, COMPANY_NUMBER, transaction.getId());
             final CompanyProfile company = CompanyProfileFixtures.generateCompanyProfile();
             company.setCompanyNumber(COMPANY_NUMBER);
             Dissolution dissolution = aDissolution().
